03ffbe20f6ce0d329102c14a5036cc1c5fd3833d
Olga Verciuc <o.verciuc@gmail.com>
2021-05-04 09:55:25 +0200

    Add new files

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # Node.js Emoji Generator
 
 Command line program to generate emoji
+
+1. print random emoji if no emoji specified
+2. print chosen emoji if user enters [shortcode](https://www.webfx.com/tools/emoji-cheat-sheet/)
+3. show error message if user enters invalid shortcode
--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-console.log('Hello');
+const emoji = require('node-emoji');
+// console.log(emoji.get('pizza'));
+// console.log(emoji.random().emoji);
+// console.log(process.argv[2]);
+// console.log(emoji.random().emoji);
+
+if (process.argv[2] === undefined) {
+    console.log(emoji.random().emoji);
+} else if (emoji.hasEmoji[process.argv[2]]) {
+    console.log(emoji.get(process.argv[2]));
+} else {
+    console.log('Emoji not found');
+}
